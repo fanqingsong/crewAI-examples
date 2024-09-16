e677801d1139161e6a2bff79cd96fe3e4ef0b09b
lightsong <qsfan@qq.com>
2024-09-16 23:15:04 +0800

    update

--- screenplay_writer/screenplay_writer.py
+++ screenplay_writer/screenplay_writer.py
@@ -1,10 +1,22 @@
-import re
+import re, os
 import yaml
 from pathlib import Path
 from crewai import Agent, Task, Crew, Process
 from dotenv import load_dotenv
+# from langchain.chat_models.openai import ChatOpenAI
+from langchain_community.chat_models.openai import ChatOpenAI
+
+
 
 load_dotenv()
+
+
+defalut_llm = ChatOpenAI(openai_api_base=os.environ.get("OPENAI_API_BASE_URL", "https://api.openai.com/v1"),
+                        openai_api_key=os.environ.get("OPENAI_API_KEY"),
+                        temperature=0.1,                        
+                        model_name=os.environ.get("MODEL_NAME", "gpt-3.5-turbo"),
+                        top_p=0.3)
+
 
 # Use Path for file locations
 current_dir = Path.cwd()
@@ -19,27 +31,84 @@ with open(tasks_config_path, "r") as file:
     tasks_config = yaml.safe_load(file)
 
 ## Define Agents
-spamfilter = Agent(
-    config=agents_config["spamfilter"], allow_delegation=False, verbose=True
+spamfilter = Agent( 
+    role=agents_config["spamfilter"]['role'], 
+    goal=agents_config["spamfilter"]['goal'], 
+    backstory=agents_config["spamfilter"]['backstory'], 
+    allow_delegation=False, 
+    verbose=True, 
+    llm=defalut_llm
 )
 
-analyst = Agent(config=agents_config["analyst"], allow_delegation=False, verbose=True)
-
-scriptwriter = Agent(
-    config=agents_config["scriptwriter"], allow_delegation=False, verbose=True
+analyst = Agent( 
+    role=agents_config["analyst"]['role'], 
+    goal=agents_config["analyst"]['goal'], 
+    backstory=agents_config["analyst"]['backstory'], 
+    allow_delegation=False, 
+    verbose=True, 
+    llm=defalut_llm
 )
 
-formatter = Agent(
-    config=agents_config["formatter"], allow_delegation=False, verbose=True
+scriptwriter = Agent( 
+    role=agents_config["scriptwriter"]['role'], 
+    goal=agents_config["scriptwriter"]['goal'], 
+    backstory=agents_config["scriptwriter"]['backstory'], 
+    allow_delegation=False, 
+    verbose=True, 
+    llm=defalut_llm
 )
 
+formatter = Agent( 
+    role=agents_config["formatter"]['role'], 
+    goal=agents_config["formatter"]['goal'], 
+    backstory=agents_config["formatter"]['backstory'], 
+    allow_delegation=False, 
+    verbose=True, 
+    llm=defalut_llm
+)
 
-scorer = Agent(config=agents_config["scorer"], allow_delegation=False, verbose=True)
+scorer = Agent( 
+    role=agents_config["scorer"]['role'], 
+    goal=agents_config["scorer"]['goal'], 
+    backstory=agents_config["scorer"]['backstory'], 
+    allow_delegation=False, 
+    verbose=True, 
+    llm=defalut_llm
+)
+
 
 
 # this is one example of a public post in the newsgroup alt.atheism
 # try it out yourself by replacing this with your own email thread or text or ...
 discussion = """From: keith@cco.caltech.edu (Keith Allan Schneider)
+Subject: Re: <Political Atheists?
+Organization: California Institute of Technology, Pasadena
+Lines: 50
+NNTP-Posting-Host: punisher.caltech.edu
+
+bobbe@vice.ICO.TEK.COM (Robert Beauchaine) writes:
+
+>>I think that about 70% (or so) people approve of the
+>>death penalty, even realizing all of its shortcomings.  Doesn't this make
+>>it reasonable?  Or are *you* the sole judge of reasonability?
+>Aside from revenge, what merits do you find in capital punishment?
+
+Are we talking about me, or the majority of the people that support it?
+Anyway, I think that "revenge" or "fairness" is why most people are in
+favor of the punishment.  If a murderer is going to be punished, people
+that think that he should "get what he deserves."  Most people wouldn't
+think it would be fair for the murderer to live, while his victim died.
+
+>Revenge?  Petty and pathetic.
+
+Perhaps you think that it is petty and pathetic, but your views are in the
+minority.
+
+
+keith
+"""
+
+oo_discussion = """From: keith@cco.caltech.edu (Keith Allan Schneider)
 Subject: Re: <Political Atheists?
 Organization: California Institute of Technology, Pasadena
 Lines: 50
@@ -97,20 +166,43 @@ violating the liberty or happiness of an innocent person?
 keith
 """
 
+
 # Filter out spam and vulgar posts
 task0 = Task(
-    description=tasks_config["task0"]["description"],
+    description=tasks_config["task0"]["description"].format(discussion=discussion),
     expected_output=tasks_config["task0"]["expected_output"],
     agent=spamfilter,
 )
-result = task0.execute()
-if "STOP" in result:
-    # stop here and proceed to next post
-    print("This spam message will be filtered out")
+
+crew = Crew(
+    agents=[spamfilter],
+    tasks=[task0],
+    verbose=True,  # Crew verbose more will let you know what tasks are being worked on, you can set it to 1 or 2 to different logging levels
+    process=Process.sequential,  # Sequential process will have tasks executed one after the other and the outcome of the previous one is passed as extra content into this next.
+)
+
+inputs = {'discussion': discussion}
+result = crew.kickoff(inputs)
+# result = crew.kickoff()
+
+print("===================== end result from crew ===================================")
+print(result)
+
+# Accessing the task output
+task_output = task0.output
+
+print(f"Task Description: {task_output.description}")
+print(f"Task Summary: {task_output.summary}")
+print(f"Raw Output: {task_output.raw}")
+if task_output.json_dict:
+    print(f"JSON Output: {json.dumps(task_output.json_dict, indent=2)}")
+if task_output.pydantic:
+    print(f"Pydantic Output: {task_output.pydantic}")
+
 
 # process post with a crew of agents, ultimately delivering a well formatted dialogue
 task1 = Task(
-    description=tasks_config["task1"]["description"],
+    description=tasks_config["task1"]["description"].format(discussion=discussion),
     expected_output=tasks_config["task1"]["expected_output"],
     agent=analyst,
 )
@@ -129,24 +221,25 @@ task3 = Task(
 crew = Crew(
     agents=[analyst, scriptwriter, formatter],
     tasks=[task1, task2, task3],
-    verbose=2,  # Crew verbose more will let you know what tasks are being worked on, you can set it to 1 or 2 to different logging levels
+    verbose=True,  # Crew verbose more will let you know what tasks are being worked on, you can set it to 1 or 2 to different logging levels
     process=Process.sequential,  # Sequential process will have tasks executed one after the other and the outcome of the previous one is passed as extra content into this next.
 )
 
-result = crew.kickoff()
-
-# get rid of directions and actions between brackets, eg: (smiling)
-result = re.sub(r"\(.*?\)", "", result)
+inputs = {'discussion': discussion}
+result = crew.kickoff(inputs)
 
 print("===================== end result from crew ===================================")
 print(result)
-print("===================== score ==================================================")
-task4 = Task(
-    description=tasks_config["task4"]["description"],
-    expected_output=tasks_config["task4"]["expected_output"],
-    agent=scorer,
-)
 
-score = task4.execute()
-score = score.split("\n")[0]  # sometimes an explanation comes after score, ignore
-print(f"Scoring the dialogue as: {score}/10")
+
+
+# print("===================== score ==================================================")
+# task4 = Task(
+#     description=tasks_config["task4"]["description"],
+#     expected_output=tasks_config["task4"]["expected_output"],
+#     agent=scorer,
+# )
+
+# score = task4.execute()
+# score = score.split("\n")[0]  # sometimes an explanation comes after score, ignore
+# print(f"Scoring the dialogue as: {score}/10")
